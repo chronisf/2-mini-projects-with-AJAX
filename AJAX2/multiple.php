e8859adf2096eee5c3dc1418f78793dc1eea0015
Chronis Fiflis <76593968+chronisf@users.noreply.github.com>
2021-01-15 15:53:57 +0200

    Add files via upload

---
+++ AJAX2/multiple.php
@@ -0,0 +1,12 @@
+<?php
+  if ($_GET["select"] == "1") {
+    echo ' Europe Hotels in Athens Διεύθυνση:Ανδρεα Μεταξα 25 τηλέφωνο:2106547654 <br>';
+	 
+  }
+  if ($_GET["select"] == "2") {
+   echo 'Europe Hotels in London Διεύθυνση:Chelsea street  τηλέφωνο:+442079460350<br>';
+  }
+if ($_GET["select"] == "3") {
+   echo 'Europe Hotels in Paris Διεύθυνση: 42 Rue Cade     τηλέφωνο:+0768990886<br>';
+  }
+?>
